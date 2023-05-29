ce1fcdc980611b8b4a9534d5d7475183daf1a146
jcndongo <mjndongo@gmail.com>
2023-05-29 19:56:27 -0400

    second commit

---
+++ OVERALL_Happy_DBox/data.js
@@ -0,0 +1,61 @@
+let data = {
+    Afghanistan: {
+        "Social_support": 0.341,
+        "Corruption": 0.847,
+    },
+
+    Lebanon: {
+        "Social_support": 0.530,
+        "Corruption": 0.891,
+    },
+
+    Zimbabwe: {
+        "Social_support": 0.690,
+        "Corruption": 0.766,
+    },
+
+    Botswana: {
+        "Social_support": 0.753,
+        "Corruption": 0.830,
+    },
+
+    Tanzania: {
+        "Social_support": 0.653,
+        "Corruption": 0.554,
+    },
+
+    Zambia: {
+        "Social_support": 0.694,
+        "Corruption": 0.818,
+    },
+
+    Sweden: {
+        "Social_support": 0.939,
+        "Corruption": 0.202,
+    },
+
+    Netherlands: {
+        "Social_support": 0.930,
+        "Corruption": 0.379,
+    },
+
+    Israel: {
+        "Social_support": 0.943,
+        "Corruption": 0.708,
+    },
+
+    Iceland: {
+        "Social_support": 0.983,
+        "Corruption": 0.668,
+    },
+
+    Denmark: {
+        "Social_support": 0.954,
+        "Corruption": 0.196,
+    },
+
+    Finland: {
+        "Social_support": 0.969,
+        "Corruption": 0.182,
+    },
+};
